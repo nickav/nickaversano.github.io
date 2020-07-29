a979c0764fcb10370f7498c7e5daa4827c2a5303
Nick Aversano <nick23hi@gmail.com>
2020-07-29 19:00:10 -0400

    updates

---
+++ src/_posts/0100-directors-cut.md
@@ -0,0 +1,9 @@
+---
+title: Director's Cut
+headline: It's berry good for you
+date: 2016-04-29 00:00:00
+image: directors-cut.jpg
+tags: video
+link: https://www.youtube.com/watch?v=Pwwsj_nlfM8
+link_only: true
+---
--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
+if (typeof posts === 'undefined') posts = [];
+if (typeof pages === 'undefined') pages = [];
+
 const content = document.getElementById('content');
 
-//content.innerHTML = posts.map(e => e.id).join('\n');
+function onLinkClick(event) {
+  const href = event.target.href;
+  const url = new URL(href);
+
+  if (url.pathname === window.location.pathname) {
+    event.preventDefault();
+    return;
+  }
+
+  history.pushState(null, '', href);
+
+  const payload = {
+    path: url.pathname,
+    method: 'PUSH',
+  };
+  route(payload);
+
+  // prevent page from reloading
+  event.preventDefault();
+}
+
+function handlePopState(event) {
+  const payload = { path: document.location.pathname, method: 'POP' };
+  route(payload);
+}
+
+function route(state) {
+  console.log('route', state);
+
+  const slug = state.path.slice(1);
+
+  let page =
+    posts.find((it) => it.slug === slug) ||
+    pages.find((it) => it.slug === slug);
+
+  console.log({ posts, pages });
+
+  if (!page) {
+    page = { title: '', html: 'Not Found!' };
+  }
+
+  document.title = titleify(page.title, 'Nick Aversano');
+  content.innerHTML = page.html;
+  bindInternalLinkListeners(content.getElementsByTagName('a'));
+}
+
+const origin = window.location.origin;
+function isExternalLink(link) {
+  return link.href.startsWith('http') && link.origin !== origin;
+}
+
+function addBlankTargetToExternalLinks() {
+  var links = document.getElementsByTagName('a');
+  for (var i = 0, n = links.length; i < n; i++) {
+    const it = links[i];
+    if (isExternalLink(it)) {
+      it.target = '_blank';
+    }
+  }
+}
+
+function bindInternalLinkListeners(links) {
+  Array.from(links).forEach((link) => {
+    if (!isExternalLink(link)) {
+      link.addEventListener('click', onLinkClick);
+    }
+  });
+}
+
+function load() {
+  addBlankTargetToExternalLinks();
+
+  const historyIsSupported = typeof history.pushState === 'function';
+
+  if (historyIsSupported) {
+    window.addEventListener('popstate', handlePopState);
+    bindInternalLinkListeners(document.getElementsByTagName('a'));
+  }
+}
+
+window.addEventListener('load', load);
